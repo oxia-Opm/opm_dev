987c200ad231b9a73b6497868076b75d3274a56f
oxia-opm <haithem.benammar@oxia-group.com>
2013-02-20 14:37:50 +0100

    * update action_project's entity

--- src/Opm/ImputationBundle/Entity/action_projet.php
+++ src/Opm/ImputationBundle/Entity/action_projet.php
@@ -12,6 +12,61 @@ use Symfony\Component\Validator\Constraints as Assert;
 class action_projet
 {
 
+  /**
+   * @ORM\Id
+   * @ORM\Column(type="integer")
+   */
+  private $action_id;
+  
+  /**
+   * @ORM\Column(type="string", length=250)
+   * @Assert\Blank()
+   */
+  private $description;
+  
+  /**
+   * @ORM\Column(type="string", length=30)
+   * @Assert\Blank()
+   */
+  private $affectee_a;
+  
+  /**
+   * @ORM\Column(type="date")
+   * @Assert\Blank()
+   */
+  private $date_soumission;
+  
+   /**
+   * @ORM\Column(type="date")
+   * @Assert\Blank()
+   */
+  private $due_le;
+  
+  /**
+   * @ORM\Column(type="integer", nullable=true)
+   * @Assert\Blank()
+   */
+  private $statut_id;
+  
+  /**
+   * @ORM\Column(type="integer", nullable=true)
+   * @Assert\NotBlank()
+   */
+  private $systeme_id;
+  
+   /**
+   * @ORM\ManyToOne(targetEntity="requete")
+   * @ORM\JoinColumn(name="requete_id", referencedColumnName="requete_id", nullable=false)
+   * @Assert\NotBlank()
+   */
+  private $requete_id;
+  
+   /**
+   * @ORM\Column(type="integer", nullable=true)
+   * @Assert\Blank()
+   */
+  private $fermee;
+
 }
 
 /*
